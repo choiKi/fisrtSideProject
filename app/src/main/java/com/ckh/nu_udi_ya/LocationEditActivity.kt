123ca5a1a96e9cebd3d5693e87652569bcc9c38f
ChoiKi <rlgns923@naver.com>
2021-09-13 17:12:52 +0900

    s

---
+++ app/src/main/java/com/ckh/nu_udi_ya/LocationEditActivity.kt
@@ -0,0 +1,13 @@
+package com.ckh.nu_udi_ya
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LocationEditActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_location_edit)
+
+
+    }
+}
--- app/src/main/java/com/ckh/nu_udi_ya/MainActivity.kt
+++ app/src/main/java/com/ckh/nu_udi_ya/MainActivity.kt
@@ -3,6 +3,9 @@ package com.ckh.nu_udi_ya
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Layout
+import android.widget.Button
+import android.widget.LinearLayout
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.ktx.Firebase
 
@@ -13,6 +16,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val logOutBtn = findViewById<LinearLayout>(R.id.logOut)
+        val locationAdd = findViewById<LinearLayout>(R.id.addLocate)
+
+        logOutBtn.setOnClickListener {
+            startActivity(Intent(this,LoginActivity::class.java))
+        }
+        locationAdd.setOnClickListener {
+            startActivity(Intent(this,LocationEditActivity::class.java))
+        }
 
     }
 
